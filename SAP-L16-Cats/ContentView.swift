010f6d9e5e23a4654af6e3ad9c0a48eba6192859
natruz <88256962+natruz@users.noreply.github.com>
2022-07-16 10:59:07 +0800

    TabView set-up

--- SAP-L16-Cats/ContentView.swift
+++ SAP-L16-Cats/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            CatFactView()
+                .tabItem {
+                    Label("Cat Facts", systemImage: "lightbulb")
+                }
+            CatImageView()
+                .tabItem {
+                    Label("Cat Image", systemImage: "photo")
+                }
+        }
     }
 }
 
