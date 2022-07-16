591be4ed3af1d44bd16b1fadfd45df8c0cf2e742
natruz <88256962+natruz@users.noreply.github.com>
2022-07-16 10:54:12 +0800

    Initial Commit

---
+++ SAP-L16-Cats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAP-L16-Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 16/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SAP-L16-Cats/SAP_L16_CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_L16_CatsApp.swift
+//  SAP-L16-Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_L16_CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
