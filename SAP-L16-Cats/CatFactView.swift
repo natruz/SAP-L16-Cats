0ec68e92558693c23e980dffe9cd272c27863d01
natruz <88256962+natruz@users.noreply.github.com>
2022-07-16 10:55:44 +0800

    Project & View set-up

---
+++ SAP-L16-Cats/CatFactView.swift
@@ -0,0 +1,20 @@
+//
+//  CatFactView.swift
+//  SAP-L16-Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 16/7/22.
+//
+
+import SwiftUI
+
+struct CatFactView: View {
+    var body: some View {
+        Text("This is cat fact view")
+    }
+}
+
+struct CatFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatFactView()
+    }
+}
---
+++ SAP-L16-Cats/CatImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CatImageView.swift
+//  SAP-L16-Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 16/7/22.
+//
+
+import SwiftUI
+
+struct CatImageView: View {
+    var body: some View {
+        Text("This is cat image view")
+    }
+}
+
+struct CatImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatImageView()
+    }
+}
